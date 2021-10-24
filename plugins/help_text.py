444f073e53ad5e0b5cb99d81c786fcee57fba576
NovaGaming1964 <40416745+NovaGaming1964@users.noreply.github.com>
2021-10-24 09:36:31 +0600

    Update help_text.py
    Deleted inline

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -47,17 +47,7 @@ async def start(bot, update):
     await AddUser(bot, update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(update.from_user.mention),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/NovaGaming1964/UPLOADER-BOT"
-                    ),
-                    
-                ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/ghostsniper7")],
-            ]
-        ),
+        text=Translation.START_TEXT,
         reply_to_message_id=update.message_id
+        
     )
